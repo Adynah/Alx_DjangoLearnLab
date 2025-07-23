e3b2665677cca81877dbb0b0a27d2e9de0f9ae3d
adynah <t.barimalaa@gmail.com>
2025-07-23 16:23:30 +0100

    User Authentication

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,7 @@ from .models import Book
 from django.views.generic.detail import DetailView
 from django.shortcuts import get_object_or_404
 from .models import Library
+from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
@@ -44,6 +45,14 @@ class UserRegisterView(CreateView):
     def form_valid(self, form):
         response = super().form_valid(form)
         return response
+    
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in after successful registration
+            return redirect('home')
+        return render(request, 'relationship_app/register.html', {'form': form})
 
 def home(request):
     return render(request, 'relationship_app/home.html')

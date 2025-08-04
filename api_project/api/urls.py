9c2f0ed0b8cb97ba011242eee3507b2f6daa8edb
adynah <t.barimalaa@gmail.com>
2025-08-04 13:29:41 +0100

    CRUD operations with ViewSet and Routers in DRF

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.db import router
+from django.urls import path, include
 from . import views
 from .views import BookList
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', views.BookList.as_view(), name="book_list_create"),
+    path('', include(router.urls)),
 ]

9a465a2e7d3c4f3d00a86fff4229b6b575e08108
adynah <t.barimalaa@gmail.com>
2025-09-09 09:18:43 +0100

    Refactor post views: merge templates and update Create, Update, Delete views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -31,10 +31,13 @@ def home_view(request):
 # List all posts
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/post_list.html'  # blog/templates/blog/post_list.html
-    context_object_name = 'posts'
-    ordering = ['-published_date']
-    paginate_by = 5
+    template_name = "blog/post.html"
+    context_object_name = "posts"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['action'] = 'list'
+        return context
 
 # Detail of a single post
 class PostDetailView(DetailView):
@@ -42,32 +45,36 @@ class PostDetailView(DetailView):
     Display a single blog post along with all its related comments.
     Authenticated users can add a new comment directly from this view.
     """
+    class PostDetailView(DetailView):
     model = Post
-    template_name = 'blog/post_detail.html'
+    template_name = "blog/post.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Add comment form to the context
-        context['comment_form'] = CommentForm()
-        context['comments'] = self.object.comments.all()  # all comments for this post
-        return context
+        context['action'] = 'detail'
+        return contex
 
 
 # Create a new post
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
-    template_name = 'blog/post_form.html'
+    template_name = 'blog/post.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['action'] = 'create'
+        return context
 
 # Update a post
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostForm
-    template_name = 'blog/post_form.html'
+    template_name = 'blog/post.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -76,13 +83,23 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['action'] = 'update'
+        return context
 
 # Delete a post
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    template_name = 'blog/post_confirm_delete.html'
+    template_name = 'blog/post.html'
     success_url = '/posts/'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['action'] = 'delete'
+        return context
+    
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author

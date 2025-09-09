72b93c793b35925e99577070c7ee01b452746a8c
adynah <t.barimalaa@gmail.com>
2025-09-09 10:11:00 +0100

    Implement user registration serializer with token creation in accounts app

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,21 +1,26 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import CustomUser
+from rest_framework.authtoken.models import Token
 from django.contrib.auth.password_validation import validate_password
 
+User = get_user_model()
+
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])  # serializers.CharField()
 
     class Meta:
-        model = CustomUser
+        model = User
         fields = ('username', 'email', 'password', 'bio', 'profile_picture')
 
     def create(self, validated_data):
-        user = CustomUser.objects.create(
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
+            password=validated_data['password'],
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
-        user.set_password(validated_data['password'])
-        user.save()
+        # Must-have: create a token for the new user
+        Token.objects.create(user=user)
         return user
+

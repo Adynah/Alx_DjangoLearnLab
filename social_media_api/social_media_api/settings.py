504f29988d5f05fa701d256c0d5635d6a4af3c79
adynah <t.barimalaa@gmail.com>
2025-09-10 01:36:54 +0100

    Change debug to false

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-for-local')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = ['adina-social-media-api.onrender.com']
 
